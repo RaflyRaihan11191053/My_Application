d7552a8dbebc7b54bbf4e10c00a41dbf3b4effcb
Rafly Raihan <raflyraihan@MacBook-Pro-Rafly.local>
2022-03-07 21:58:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val name = "Rafly"
+        Log.d("AND-4", "Selamat pagi!, $name")
+    }
+}
