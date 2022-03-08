6bec85abdec441375e8a87ff4c4c510c70b480cf
raflyraihan <11191053@student.itk.ac.id>
2022-03-08 20:15:16 +0800

    Add some code

---
+++ app/src/main/java/com/example/myapplication/LayoutGojek.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+class LayoutGojek : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_gojek)
+    }
+}
